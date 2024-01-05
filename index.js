87430f1cec7a394379eac9af5936cc06afe10039
Goncalo Gouveia <goncalo.miguel.gouveia@gmail.com>
2024-01-05 20:32:03 +0000

    geggtes\    \\

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require ('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+const commentRouter = require('./comments/commentRouter');
+const userRouter = require('./users/UserRouter');
+const productRouter = require('./products/ProductsRouter');
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/comments', commentRouter);
+
+const conection = "mongodb+srv://goncalomGouveia:Gouveia12345@techof.00ekxg0.mongodb.net/Armazem?retryWrites=true&w=majority"
+
+mongoose.connect(conection,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const port = 3000
+//routes
+
+app.listen(port);
